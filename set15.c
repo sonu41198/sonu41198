42f4858f74a7a5a3cfba36e4289582592c6ce616
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2018-12-22 15:42:39 +0530

    Create set15.c

---
+++ set15.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the 3 number's:");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b && a>c)
+	{
+		printf("%d is greater",a);
+	}
+	else if(b>a && b>c)
+	{
+		printf("%d is greater",b);
+	}
+	else
+	{
+		printf("%d is greater",c);
+	}
+	return 0;
+}
