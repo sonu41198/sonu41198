66d64cfe55f51431d9ddd974b92f8d7461686e80
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2018-12-22 20:02:44 +0530

    Create set16.c

---
+++ set16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int year;
+	printf("enter the year:");
+	scanf("%d",&year);
+	if(year%400==0 || year%4==0 && year%100!=0)
+	{
+	printf("%d is leap year",year);
+	}
+	else
+	{
+		printf("%d is not leap year",year);
+	}
+	return 0;
+}
