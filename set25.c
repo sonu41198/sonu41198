9e2e890a01352605553d596ae398bdb5e40b1899
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2018-12-25 15:20:54 +0530

    Create set25.c

---
+++ set25.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,i;
+	printf("enter the two numbers:");
+	scanf("%d %d",&a,&b);
+	for(i=a+1;i<b;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d\t",i);
+		}	
+	}
+	return 0;
+}
