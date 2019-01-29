e138f6c6c7158a8d715981619bbf46db9fb61a2b
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-01-29 20:43:49 +0530

    Create set3-9.c

---
+++ set3-9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ( void )
+{
+    int numMinutes;
+    scanf("%d",&numMinutes);
+if ( (numMinutes / 60) == 1 ) 
+printf("%01d hour",numMinutes/60);
+else 
+printf("%01d hours",numMinutes/60);
+if ( (numMinutes % 60) == 1) 
+printf(" %01d minute\n",numMinutes%60);
+else 
+printf(" %01d minutes\n",numMinutes%60);
+return 0;
+}
