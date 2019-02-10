36c8d5fa1dc436c4b68244c0d191c8e65c46695c
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-10 21:13:07 +0530

    Create set6-3.c

---
+++ set6-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+	int n,temp,sum=0;
+	printf("enter the number");
+	scanf("%d",&n);
+	temp=n;
+	while(n!=0)
+	{
+		temp=n%10;
+		sum=sum+temp;
+		n=n/10;
+ 
+	}
+	printf("\n%d",sum);
+	return 0;
+}
