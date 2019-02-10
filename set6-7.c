edbb5cf06338e0489de1f79a5f7e3b188cf873d2
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-10 21:52:18 +0530

    Update set6-7.c

--- set6-7.c
+++ set6-7.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int t,a,b;
-    scanf("%d %d",&a,&b);
-    t=a;
-    a=b;
-    b=t;
-    printf("a:%d\nb:%d",a,b);
+    int count=0,n,num,i,a[50];
+    scanf("%d %d",&n,&num);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==num)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
     return 0;
 }
