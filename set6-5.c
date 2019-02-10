43724bc738190683813b301ffa01a669aed2aba5
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-10 21:27:30 +0530

    Update set6-5.c

--- set6-5.c
+++ set6-5.c
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+int main()
+{
+    int num,m,res;
+    scanf("%d",&num);
+    scanf("%d",&m);
+    res=num*m;
+    if(res%2==0)
+    {
+        printf("%d is even",res);
+    }
+    else
+    {
+        printf("%d is odd",res);
+    }
+    return 0;
+}
