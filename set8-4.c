6a523eb94576b18afb72a5ffabdd8015f66deaf4
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-11 07:09:03 +0530

    Create set8-4.c

---
+++ set8-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) 
+{
+	float num;
+	int n;
+	printf("\n enter the num:");
+	scanf("%f",&num);
+	n=(int)(num+0.5);
+	printf("\n%d",n);
+	return 0;
+}
