d0dd1c4ffa5288358a71f193e99287e732c591d6
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-11 11:06:00 +0530

    Create set8-6.c

---
+++ set8-6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,c=2;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c>2)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
