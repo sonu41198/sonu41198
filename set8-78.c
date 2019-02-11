200d15697f5c79d706c68930ba6cb225939a109c
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-11 11:20:03 +0530

    Create set8-78.c

---
+++ set8-78.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+        if(n%13==0)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    return 0;
+}
