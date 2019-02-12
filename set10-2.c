b25660f1a25034562657292d7e6adc81871ce2db
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-12 14:30:50 +0530

    Update set10-2.c

--- set10-2.c
+++ set10-2.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main(void) 
 {
-	int n,a[50],i,sum=0;
+	int x,a[50],i,sum=0;
 	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	for(i=0;i<x;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i<x;i++)
 	{
 		sum=sum+a[i];	
 	}
